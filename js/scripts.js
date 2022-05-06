90c6103fc95027d44bfae5cae2c89a7f32fbb333
R. Fariz <farizramazanov38@gmail.com>
2022-05-06 16:50:30 +0400

    feat: task1 console project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 "use strict";
 
-// let res = confirm("Are you here");
-// console.log(res);
-// let res = prompt("fd", "");
-// console.log(res);
+const numberOfFilms = +prompt("How many films did you watch?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("What film did you watch last?", ""),
+      b = prompt("How it was?", ""),
+      c = prompt("What film did you watch last?", ""),
+      d = prompt("How it was?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
