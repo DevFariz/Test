cbec878e19647a02f3597998afce9bc76b87a1d9
R. Fariz <farizramazanov38@gmail.com>
2022-05-08 11:29:12 +0400

    feat: add functions to app

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many films did you watch?", "");
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt("How many films did you watch?", "");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("How many films did you watch?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,19 +20,35 @@ const personalMovieDB = {
     privat: false
 };
 
-
-
-for (let index = 0; index < 2; index++) {
-    const a = prompt("What film did you watch last?", ""),
-          b = prompt("How it was?", "");
-
-    if(a != null && b != null && a != "" && b != "" && a.length < 50){
-        personalMovieDB.movies[a] = b;
-    } else{
-        i--;
-    }
+function rememberMyFilms(){
+    for (let index = 0; index < 2; index++) {
+        const a = prompt("What film did you watch last?", ""),
+              b = prompt("How it was?", "");
     
+        if(a != null && b != null && a != "" && b != "" && a.length < 50){
+            personalMovieDB.movies[a] = b;
+        } else{
+            index--;
+        }
+    }
 }
 
-console.log(personalMovieDB);
+// rememberMyFilms();
 
+
+function showMyDB(obj){
+    if(!obj.privat){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB);
+
+function writeYourGenres(obj){
+    for(let i = 1; i <= 3; i++){
+        const genre = prompt("Enter a genre: ");
+        obj.genres[i - 1] = genre;
+    }
+}
+
+console.log(writeYourGenres(personalMovieDB));
